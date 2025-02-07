c5659ade0be925231199e266e6f0700799d4558d
00162317 <hernandez.roberto014@gmai.com>
2025-02-07 15:10:58 -0600

    Se cambio columnas de efectivo, transferencia, t/c, y se agregaron condiciones en el where como que ignore los pagos anulados

--- cierreCaja.sql
+++ cierreCaja.sql
@@ -2,10 +2,18 @@
 ------------COBROS
 --1. PAGOS RECIBIDOS DE CONTADO
 select  T0."DocEntry", T0."CardCode", T3."NumAtCard", T0."CardName", T0."DocDate", T0."TaxDate", 
-         T0."DocTotal", T3."DocNum", '0' ,'3' DocType , 'PAGOS RECIBIDOS DE CONTADO', T3."GroupNum", T4."SeriesName",T0."UserSign"   
-        ,T0."CashSum",T0."CreditSum", T0."CheckSum", T0."TrsfrSum", T5."U_NAME", CASE WHEN SUBSTRING(T4."SeriesName",5,2) = 'JP' THEN 'JUAN PABLO'
+         T3."DocTotal", T3."DocNum", '0' ,'3' DocType , 'PAGOS RECIBIDOS DE CONTADO', T3."GroupNum", T4."SeriesName",T0."UserSign"   
+        ,(case when T0."Canceled"='Y' THEN 0 
+            when T1."SumApplied" != T0."CashSum" AND T0."CashSum"> 0  THEN T1."SumApplied" 
+            else T0."CashSum" end)  Efectivo,(case when T0."Canceled"='Y' then '0'
+            when T1."SumApplied" != T0."CreditSum" AND T0."CreditSum" > 0  THEN T1."SumApplied" 
+             else T0."CreditSum" end)  Tarjeta, (case when T0."Canceled"='Y' then '0' 
+            when T1."SumApplied" != T0."CheckSum" AND T0."CheckSum" > 0  THEN T1."SumApplied" 
+            else T0."CheckSum" end)  Cheque,  (case when T0."Canceled"='Y' then '0' 
+            when T1."SumApplied" != T0."TrsfrSum" AND T0."TrsfrSum" > 0  THEN T1."SumApplied" 
+            else T0."TrsfrSum" end)  Transferencia, T5."U_NAME", CASE WHEN SUBSTRING(T4."SeriesName",5,2) = 'JP' THEN 'JUAN PABLO'
         WHEN SUBSTRING(T4."SeriesName",5,2) = 'ML' THEN 'MERLIOT'
-        WHEN SUBSTRING(T4."SeriesName",5,2) = 'SM' THEN 'SAN MIGUEL' ELSE SUBSTRING(T4."SeriesName",5,2) END Sucursal, '0', '0', T0."VatSum"
+        WHEN SUBSTRING(T4."SeriesName",5,2) = 'SM' THEN 'SAN MIGUEL' ELSE SUBSTRING(T4."SeriesName",5,2) END Sucursal, '0', '0', T0."VatSum", 'A'
 
 from ORCT  T0               
 
@@ -17,16 +25,24 @@ from ORCT  T0
 
         LEFT JOIN "OUSR" T5 ON T0."UserSign" = T5."USERID"   
 
-where T0."DocDate"={?fechaCierre} and T5."U_NAME"='{?CodUser}' and T0."Canceled" = 'N'
+where T0."DocDate"={?fechaCierre} and T5."U_NAME"='{?CodUser}' and T0."Canceled" = 'N' and T3."GroupNum" IN (3) 
 
 UNION ALL
 --2. COBROS DE CREDITO
 
 select  T0."DocEntry", T0."CardCode", T3."NumAtCard", T0."CardName", T0."DocDate", T0."TaxDate", 
-         T0."DocTotal",  T3."DocNum", '0' ,'3' DocType , 'COBROS DE CREDITO', T3."GroupNum", T4."SeriesName",T0."UserSign"   
-        ,T0."CashSum",T0."CreditSum", T0."CheckSum", T0."TrsfrSum",T5."U_NAME", CASE WHEN SUBSTRING(T4."SeriesName",5,2) = 'JP' THEN 'JUAN PABLO'
+         T3."DocTotal",  T3."DocNum", '0' ,'3' DocType , 'COBROS DE CREDITO', T3."GroupNum", T4."SeriesName",T0."UserSign"   
+        ,(case when T0."Canceled"='Y' THEN 0 
+            when T1."SumApplied" != T0."CashSum" AND T0."CashSum"> 0  THEN T1."SumApplied" 
+            else T0."CashSum" end)  Efectivo,(case when T0."Canceled"='Y' then '0'
+            when T1."SumApplied" != T0."CreditSum" AND T0."CreditSum" > 0  THEN T1."SumApplied" 
+             else T0."CreditSum" end)  Tarjeta, (case when T0."Canceled"='Y' then '0' 
+            when T1."SumApplied" != T0."CheckSum" AND T0."CheckSum" > 0  THEN T1."SumApplied" 
+            else T0."CheckSum" end)  Cheque,  (case when T0."Canceled"='Y' then '0' 
+            when T1."SumApplied" != T0."TrsfrSum" AND T0."TrsfrSum" > 0  THEN T1."SumApplied" 
+            else T0."TrsfrSum" end)  Transferencia,T5."U_NAME", CASE WHEN SUBSTRING(T4."SeriesName",5,2) = 'JP' THEN 'JUAN PABLO'
         WHEN SUBSTRING(T4."SeriesName",5,2) = 'ML' THEN 'MERLIOT'
-        WHEN SUBSTRING(T4."SeriesName",5,2) = 'SM' THEN 'SAN MIGUEL' ELSE SUBSTRING(T4."SeriesName",5,2) END Sucursal, '0', '0', T0."VatSum"
+        WHEN SUBSTRING(T4."SeriesName",5,2) = 'SM' THEN 'SAN MIGUEL' ELSE SUBSTRING(T4."SeriesName",5,2) END Sucursal, '0', '0', T0."VatSum", 'B'
 
 from ORCT  T0               
 
@@ -38,15 +54,23 @@ from ORCT  T0
 
         LEFT JOIN "OUSR" T5 ON T0."UserSign" = T5."USERID"   
 
-where T0."DocDate"={?fechaCierre} and T5."U_NAME"='{?CodUser}' and T3."GroupNum" IN (1,2,4,5,6,7,8)
+where T0."DocDate"={?fechaCierre} and T5."U_NAME"='{?CodUser}' and T3."GroupNum" IN (1,2,4,5,6,7,8) and T0."Canceled" = 'N'
 
 UNION ALL
 --3. ANTICIPOS DE CLIENTES RECIBIDOS
 select  T0."DocEntry", T0."CardCode", T3."NumAtCard", T0."CardName", T0."DocDate", T0."TaxDate", 
-         T0."DocTotal",  T3."DocNum", '0' ,'3' DocType , 'COBROS DE CREDITO', T3."GroupNum", T4."SeriesName",T0."UserSign"   
-        ,T0."CashSum",T0."CreditSum", T0."CheckSum", T0."TrsfrSum",T5."U_NAME", CASE WHEN SUBSTRING(T4."SeriesName",5,2) = 'JP' THEN 'JUAN PABLO'
+         T3."DocTotal",  T3."DocNum", '0' ,'3' DocType , 'ANTICIPOS DE CLIENTES RECIBIDOS', T3."GroupNum", T4."SeriesName",T0."UserSign"   
+        ,(case when T0."Canceled"='Y' THEN 0 
+            when T1."SumApplied" != T0."CashSum" AND T0."CashSum"> 0  THEN T1."SumApplied" 
+            else T0."CashSum" end)  Efectivo,(case when T0."Canceled"='Y' then '0'
+            when T1."SumApplied" != T0."CreditSum" AND T0."CreditSum" > 0  THEN T1."SumApplied" 
+             else T0."CreditSum" end)  Tarjeta, (case when T0."Canceled"='Y' then '0' 
+            when T1."SumApplied" != T0."CheckSum" AND T0."CheckSum" > 0  THEN T1."SumApplied" 
+            else T0."CheckSum" end)  Cheque,  (case when T0."Canceled"='Y' then '0' 
+            when T1."SumApplied" != T0."TrsfrSum" AND T0."TrsfrSum" > 0  THEN T1."SumApplied" 
+            else T0."TrsfrSum" end)  Transferencia,T5."U_NAME", CASE WHEN SUBSTRING(T4."SeriesName",5,2) = 'JP' THEN 'JUAN PABLO'
         WHEN SUBSTRING(T4."SeriesName",5,2) = 'ML' THEN 'MERLIOT'
-        WHEN SUBSTRING(T4."SeriesName",5,2) = 'SM' THEN 'SAN MIGUEL' ELSE SUBSTRING(T4."SeriesName",5,2) END Sucursal, '0', '0', T0."VatSum"
+        WHEN SUBSTRING(T4."SeriesName",5,2) = 'SM' THEN 'SAN MIGUEL' ELSE SUBSTRING(T4."SeriesName",5,2) END Sucursal, '0', '0', T0."VatSum", 'C'
 
 from ORCT  T0               
 
@@ -58,7 +82,7 @@ from ORCT  T0
 
         LEFT JOIN "OUSR" T5 ON T0."UserSign" = T5."USERID"   
 
-where T0."DocDate"={?fechaCierre} and T5."U_NAME"='{?CodUser}' and T0."PayNoDoc" = 'Y'
+where T0."DocDate"={?fechaCierre} and T5."U_NAME"='{?CodUser}' and T0."PayNoDoc" = 'Y' and T0."Canceled" = 'N'
 
 ------------FACTURACION
 
@@ -73,7 +97,7 @@ select T0."DocEntry", T0."CardCode", T0."NumAtCard", T0."CardName", T0."DocDate"
         WHEN SUBSTRING(T1."SeriesName",5,2) = 'ML' THEN 'MERLIOT'
         WHEN SUBSTRING(T1."SeriesName",5,2) = 'SM' THEN 'SAN MIGUEL' ELSE SUBSTRING(T1."SeriesName",5,2) END Sucursal,
         CASE WHEN T0."GroupNum" IN (1,2,4,5,6,7,8) THEN T0."DocTotal" ELSE 0 END Credito,
-        CASE WHEN T0."GroupNum" IN (3) THEN T0."DocTotal" ELSE 0 END Contado, T0."VatSum"
+        CASE WHEN T0."GroupNum" IN (3) THEN T0."DocTotal" ELSE 0 END Contado, T0."VatSum", 'D'
 from OINV T0 
 
         left join NNM1 T1 on T0."Series"=T1."Series"
@@ -92,7 +116,7 @@ select T0."DocEntry", T0."CardCode", T0."NumAtCard", T0."CardName", T0."DocDate"
         WHEN SUBSTRING(T1."SeriesName",5,2) = 'ML' THEN 'MERLIOT'
         WHEN SUBSTRING(T1."SeriesName",5,2) = 'SM' THEN 'SAN MIGUEL' ELSE SUBSTRING(T1."SeriesName",5,2) END Sucursal,
         CASE WHEN T0."GroupNum" IN (1,2,4,5,6,7,8) THEN T0."DocTotal" ELSE 0 END Credito,
-        CASE WHEN T0."GroupNum" IN (3) THEN T0."DocTotal" ELSE 0 END Contado, T0."VatSum"
+        CASE WHEN T0."GroupNum" IN (3) THEN T0."DocTotal" ELSE 0 END Contado, T0."VatSum", 'E'
 from OINV T0 
 
         left join NNM1 T1 on T0."Series"=T1."Series"
@@ -114,7 +138,7 @@ select T0."DocEntry",  T0."CardCode", T0."NumAtCard", T0."CardName", T0."DocDate
         WHEN SUBSTRING(T1."SeriesName",5,2) = 'ML' THEN 'MERLIOT'
         WHEN SUBSTRING(T1."SeriesName",5,2) = 'SM' THEN 'SAN MIGUEL' ELSE SUBSTRING(T1."SeriesName",5,2) END Sucursal,
         CASE WHEN T0."GroupNum" IN (1,2,4,5,6,7,8) THEN T0."DocTotal" ELSE 0 END Credito,
-        CASE WHEN T0."GroupNum" IN (3) THEN T0."DocTotal" ELSE 0 END Contado, T0."VatSum"
+        CASE WHEN T0."GroupNum" IN (3) THEN T0."DocTotal" ELSE 0 END Contado, T0."VatSum", 'F'
 
 from ORIN  T0 
 
